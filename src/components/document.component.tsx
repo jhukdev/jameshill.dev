18e382cc6091a4ef89f1c902136101f95d28a1ce
James Hill <me@jhdev.uk>
2020-05-22 00:01:33 +0100

    Removed whitespace around GA code block

--- src/components/document.component.tsx
+++ src/components/document.component.tsx
@@ -58,12 +58,16 @@ function DocumentMeta({ title, description, lang = 'en', meta = [] }: IProps) {
         async
         src="https://www.googletagmanager.com/gtag/js?id=UA-167321875-1"
       ></script>
-      <script>{`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'UA-167321875-1');
-      `}</script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-167321875-1');
+          `,
+        }}
+      />
     </Helmet>
   );
 }
