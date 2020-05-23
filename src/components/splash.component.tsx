4255fe50e5a6ed13490fcad6b0b96c84ed330852
James Hill <me@jhdev.uk>
2020-05-23 23:27:29 +0100

    Updated layout width and splash

--- src/components/splash.component.tsx
+++ src/components/splash.component.tsx
@@ -10,9 +10,18 @@ import style from './splash.module.scss';
 function Splash() {
   return (
     <section className={style.splash}>
-      <div className={style.content}>
-        <h1 className={style.title}>Trying to build a better UI...</h1>
-      </div>
+      <h1 className={style.title}>Trying to build a better UI</h1>
+      {/* <div className={style.content}>
+        <div className={style.column}>
+          <h2 className={style.heading}>Recent Projects</h2>
+        </div>
+        <div className={style.column}>
+          <h2 className={style.heading}>Experiments</h2>
+        </div>
+        <div className={style.column}>
+          <h2 className={style.heading}>Blog</h2>
+        </div>
+      </div> */}
     </section>
   );
 }
